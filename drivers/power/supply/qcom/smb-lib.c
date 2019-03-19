65a60a00eebc8b8fe2e87bac3322efa3e16c5303
msdx321 <msdx321@gmail.com>
2019-03-19 08:59:22 -0400

    power: smb-lib: Allow user to skip thermal throttling when charging
    * Current can be capped by some other sources. Don't bother with them
    for now.
    
    Change-Id: I6e9f9072ed5bb19dfc53732242bcfac4ce688448
    Signed-off-by: msdx321 <msdx321@gmail.com>
    Signed-off-by: Yousef <yusufgadrie@gmail.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -21,6 +21,7 @@
 #include <linux/input/qpnp-power-on.h>
 #include <linux/irq.h>
 #include <linux/pmic-voter.h>
+#include <linux/module.h>
 #include "smb-lib.h"
 #include "smb-reg.h"
 #include "battery.h"
@@ -58,6 +59,10 @@ extern int hwc_check_global;
 #endif
 
 #ifdef CONFIG_MACH_MI
+
+bool skip_thermal = false;
+module_param(skip_thermal, bool, 0644);
+
 static bool off_charge_flag;
 #endif
 
@@ -2578,10 +2583,16 @@ extern int hwc_check_india;
 static int smblib_therm_charging(struct smb_charger *chg)
 {
 	int thermal_icl_ua = 0;
+	int temp_level;
 	int rc;
 
 	if (chg->system_temp_level >= MAX_TEMP_LEVEL)
 		return 0;
+
+	if (skip_thermal) {
+		temp_level = chg->system_temp_level;
+		chg->system_temp_level = 0;
+	}
 
 	switch (chg->usb_psy_desc.type) {
 	case POWER_SUPPLY_TYPE_USB_HVDCP:
@@ -2641,6 +2652,10 @@ static int smblib_therm_charging(struct smb_charger *chg)
 		if (rc < 0)
 			pr_err("Couldn't disable USB thermal ICL vote rc=%d\n",
 				rc);
+	}
+
+	if (skip_thermal) {
+		chg->system_temp_level = temp_level;
 	}
 
 	return rc;
