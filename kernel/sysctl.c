26f33a7ab33d0d5f9bb270c39a43a50cc73ce3a4
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-25 23:38:14 -0700

    sysctl: Nuke sched_boost feature
    This is perhaps the worst performance-related feature ever, and is
    thoroughly abused by userspace in all the worst ways possible.
    Triggering a massive task migration via sched_boost during
    latency-critical operations such as app launches is a very bad idea, and
    leads to noticeably-horrible stuttering.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: baibhab <fakebai34@gmail.com>
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -532,6 +532,7 @@ static struct ctl_table kern_table[] = {
 		 */
 		.extra2		= &one_thousand,
 	},
+	#if 0
 	{
 		.procname	= "sched_boost",
 		.data		= &sysctl_sched_boost,
@@ -541,6 +542,7 @@ static struct ctl_table kern_table[] = {
 		.extra1         = &zero,
 		.extra2		= &three,
 	},
+	#endif
 	{
 
 		.procname	= "sched_short_burst_ns",
