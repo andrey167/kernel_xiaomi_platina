7c52f053ffa60bcf89a4f0789b7526e3598ff1ee
andrey167 <andrey_167@bk.ru>
2026-09-17 18:10:02 +0400

    qcacld-3.0: Fix duplicate QDF debugfs initialization
    Fix duplicate QDF debugfs initialization in WLAN driver
    
    Remove the redundant qdf_debugfs_init() call from hdd_qdf_init() in wlan_hdd_main.c.
    
    QDF already initializes debugfs through qdf_module.c. The second initialization caused debugfs_create_dir() to fail when the directory already existed, resulting in QDF_STATUS_E_FAILURE and preventing the WLAN driver from completing initialization.
    
    This fixes Wi-Fi initialization and allows the wlan0 interface to be created.

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -14784,11 +14784,7 @@ static QDF_STATUS hdd_qdf_init(void)
 	if (QDF_IS_STATUS_ERROR(status))
 		goto exit;
 
-	status = qdf_debugfs_init();
-	if (QDF_IS_STATUS_ERROR(status)) {
-		hdd_err("Failed to init debugfs; status:%u", status);
-		goto print_deinit;
-	}
+
 
 	qdf_lock_stats_init();
 	qdf_mem_init();
@@ -14823,20 +14819,21 @@ static QDF_STATUS hdd_qdf_init(void)
 cpuhp_deinit:
 	qdf_cpuhp_deinit();
 talloc_deinit:
-	qdf_talloc_feature_deinit();
+        qdf_talloc_feature_deinit();
+
 event_deinit:
-	qdf_event_list_destroy();
-	qdf_mc_timer_manager_exit();
-	qdf_periodic_work_feature_deinit();
-	qdf_delayed_work_feature_deinit();
-	qdf_mem_exit();
-	qdf_lock_stats_deinit();
-	qdf_debugfs_exit();
-print_deinit:
-	hdd_qdf_print_deinit();
+        qdf_event_list_destroy();
+        qdf_mc_timer_manager_exit();
+        qdf_periodic_work_feature_deinit();
+        qdf_delayed_work_feature_deinit();
+        qdf_mem_exit();
+        qdf_lock_stats_deinit();
+        qdf_debugfs_exit();
+
+        hdd_qdf_print_deinit();
 
 exit:
-	return status;
+        return status;
 }
 
 static void hdd_qdf_deinit(void)
